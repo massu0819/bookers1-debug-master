ab71db3daffd6d8ae5068b3fdb291f12d7afafa3
massu0819 <massu4869@gmail.com>
2021-11-01 05:18:32 +0000

    [Finish]応用課題1

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_09_15_014227) do
 
   create_table "books", force: :cascade do |t|
-    t.text "title"
+    t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
